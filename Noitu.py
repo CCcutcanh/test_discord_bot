48ab42253fba80abef6297bf0b5b173edd04a6b6
CCcutcanh <91305214+CCcutcanh@users.noreply.github.com>
2022-09-22 22:56:54 +0700

    Update Noitu.py

--- Noitu.py
+++ Noitu.py
@@ -30,3 +30,38 @@ async def noitu(ctx, arg = None):
         data[str(ctx.message.guild.id)]['noitu']['channel'] = str(ctx.message.channel.id)
         await save_member_data(data)
         await ctx.send(f"đã bắt đầu trò chơi hãy bắt đầu bằng một từ bất kì")
+@bot.event
+async def on_message(message):
+    data = await get_bank_data()
+    if 'noitu' in data[str(message.guild.id)]:
+        success = True
+        if str(message.channel.id) == data[str(message.guild.id)]['noitu']['channel'] and str(message.author.id) != str(bot.user.id):
+            r = requests.get('https://raw.githubusercontent.com/undertheseanlp/dictionary/master/dictionaries/hongocduc/words.txt').text.split('\n')
+            if len(str(message.content.lower()).split(' ')) == 2 and str(message.author.id) != str(bot.user.id):
+                if message.content.lower() not in word:
+                    await message.add_reaction("❎")
+                    await message.channel.send(f'Từ `{message.content.lower()}` không tồn tại trong từ điển của bot')
+                elif message.content.lower() in data[str(message.guild.id)]['noitu']['word']:
+                    await message.channel.send(f'Từ `{message.content.lower()}` đã được nối từ trước đó')
+                try:
+                    if data[str(message.guild.id)]['noitu']['pre_word'] != str(message.content.lower()).split(' ')[0]:
+                        await message.channel.send(f"từ của bạn phải bắt đầu bằng chữ `{data[str(message.guild.id)]['noitu']['pre_word']}`")
+                        success = False
+                    if data[str(message.guild.id)]['noitu']['pre_player'] == str(message.author.id):
+                        await message.channel.send(f"Bạn đã nối trước đó hãy đợi người tiếp theo")
+                        success = False
+                except Exception as e:
+                    data[str(message.guild.id)]['noitu']['pre_word'] = str(message.content.lower()).split(' ')[1]
+                    data[str(message.guild.id)]['noitu']['pre_player'] = str(message.author.id)
+                    save_member_data(data)
+                    await message.add_reaction("✅")
+                    print(e)
+                    return
+                else:
+                    if success == True:
+                        data[str(message.guild.id)]['noitu']['word'].append(str(message.content.lower()))
+                        save_member_data(data)
+                        await message.add_reaction("✅")
+            else:
+                await message.channel.send(f'Chỉ được nối từ có 2 chữ')
+    await bot.process_commands(message)
